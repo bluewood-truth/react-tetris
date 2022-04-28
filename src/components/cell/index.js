f51cf64fd278be6871e9fe1aa7b211b2039f0829
bluewood <bluewood.truth@gmail.com>
2022-04-28 21:17:09 +0900

    add lock effect

--- src/components/cell/index.js
+++ src/components/cell/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './styles.css';
 
-export const Cell = ({color, isEmpty}) => {
+export const Cell = ({color, isEmpty, isLock}) => {
   return (
     <div
       className={styles.cell}
       style={isEmpty ? {opacity: 0} : {backgroundColor: color}}
+      data-is-lock={isLock}
     ></div>
   );
 };
--- src/components/playfield/index.js
+++ src/components/playfield/index.js
@@ -4,16 +4,17 @@ import styles from './styles.css';
 
 export const Playfield = ({field, onKeyDown}) => {
   return (
-    <div
-      className={styles.playfield}
-      tabIndex={0}
-      onKeyDown={onKeyDown}
-    >
+    <div className={styles.playfield} tabIndex={0} onKeyDown={onKeyDown}>
       <div className={styles.playfieldInner}>
         {field.map((row, i) => (
           <Row key={i}>
             {row.map((cell, j) => (
-              <Cell key={j} color={cell.color} isEmpty={cell.isEmpty} />
+              <Cell
+                key={j}
+                color={cell.color}
+                isEmpty={cell.isEmpty}
+                isLock={cell.isLock}
+              />
             ))}
           </Row>
         ))}
