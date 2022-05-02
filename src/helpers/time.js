111a02f7661c1cd331c92bc019c0171f5c88346b
bluewood <bluewood.truth@gmail.com>
2022-05-02 22:07:34 +0900

    fix an issue that time is displayed differently than it actually is

--- src/helpers/time.js
+++ src/helpers/time.js
@@ -1,11 +1,11 @@
 export const secondToMMSS = (sec) => {
-  const mm = String(Math.round(sec / 60)).padStart(2, '0');
+  const mm = String(Math.floor(sec / 60)).padStart(2, '0');
   const ss = String(sec % 60).padStart(2, '0');
   return `${mm}:${ss}`;
 };
 
 export const milliSecondToMMSSFF = (milliSec) => {
-  const mmss = secondToMMSS(Math.round(milliSec / 1000));
-  const ff = String(Math.round((milliSec % 1000) / 10)).padStart(2, '0');
+  const mmss = secondToMMSS(Math.floor(milliSec / 1000));
+  const ff = String(Math.floor((milliSec % 1000) / 10)).padStart(2, '0');
   return `${mmss}.${ff}`;
 };
